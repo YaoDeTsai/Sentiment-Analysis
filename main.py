e229ada0c320045bdc42142a8a4918509b070e8d
蔡耀德 <105727664+YaoDeTsai@users.noreply.github.com>
2024-04-20 18:33:39 +0800

    edition

---
+++ main.py
@@ -0,0 +1,87 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from nltk.corpus import stopwords 
+from collections import Counter
+import string
+import re
+import seaborn as sns
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
+
+# nltk.download('stopwords')
+
+
+is_cuda = torch.cuda.is_available()
+
+# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
+if is_cuda:
+    device = torch.device("cuda")
+    print("GPU is available")
+else:
+    device = torch.device("cpu")
+    print("GPU not available, CPU used")
+
+
+base_csv = './IMDB Dataset/IMDB Dataset.csv'
+df = pd.read_csv(base_csv)
+df.head()
+
+
+
+X,y = df['review'].values,df['sentiment'].values
+x_train,x_test,y_train,y_test = train_test_split(X,y,stratify=y,random_state=10)
+print(f'shape of train data is {x_train.shape}')
+print(f'shape of test data is {x_test.shape}')
+
+dd = pd.Series(y_train).value_counts()
+sns.barplot(x=np.array(['negative','positive']),y=dd.values)
+plt.show()
+
+def preprocess_string(s):
+    # Remove all non-word characters (everything except numbers and letters)
+    s = re.sub(r"[^\w\s]", '', s)
+    # Replace all runs of whitespaces with no space
+    s = re.sub(r"\s+", '', s)
+    # replace digits with no space
+    s = re.sub(r"\d", '', s)
+
+    return s
+
+
+def tockenize(x_train,y_train,x_val,y_val):
+    word_list = []
+
+    stop_words = set(stopwords.words('english')) 
+    for sent in x_train:
+        for word in sent.lower().split():
+            word = preprocess_string(word)
+            if word not in stop_words and word != '':
+                word_list.append(word)
+
+    corpus = Counter(word_list)
+    # sorting on the basis of most common words
+    corpus_ = sorted(corpus,key=corpus.get,reverse=True)[:1000]
+    # creating a dict
+    onehot_dict = {w:i+1 for i,w in enumerate(corpus_)}
+    
+    # tockenize
+    final_list_train,final_list_test = [],[]
+    for sent in x_train:
+            final_list_train.append([onehot_dict[preprocess_string(word)] for word in sent.lower().split() 
+                                     if preprocess_string(word) in onehot_dict.keys()])
+    for sent in x_val:
+            final_list_test.append([onehot_dict[preprocess_string(word)] for word in sent.lower().split() 
+                                    if preprocess_string(word) in onehot_dict.keys()])
+            
+    encoded_train = [1 if label =='positive' else 0 for label in y_train]  
+    encoded_test = [1 if label =='positive' else 0 for label in y_val] 
+    # add dtype=object
+    return np.array(final_list_train,dtype=object), np.array(encoded_train),np.array(final_list_test,dtype=object), np.array(encoded_test),onehot_dict
+
+np.array(final_list_test[:2],dtype=object)
+x_train,y_train,x_test,y_test,vocab = tockenize(x_train,y_train,x_test,y_test)
